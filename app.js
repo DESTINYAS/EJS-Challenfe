ebf154581700fdace6a1b8deee307c1725f9bc5a
DESTINYAS <destiny.ochonogor@gmail.com>
2022-09-03 16:50:00 +0100

    Adding few comments to app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-//jshint esversion:6
-
+//Require the necessary node modules
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -17,13 +17,17 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+//use express.static to be able to use local files
 app.use(express.static("public"));
+
 mongoose.connect("mongodb://localhost:27017/ejscDB", { useNewUrlParser: true });
 
+//Define the mongoose Schema
 const postSchema = mongoose.Schema({
   title: String,
   content: String,
 });
+// Compile the Schema into a model
 const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function (req, res) {
